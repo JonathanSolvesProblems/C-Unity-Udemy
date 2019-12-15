f40c0353b55f681399c47cfed8d2c96ca909e59e
JonathanSolvesProblems <46333281+JonathanSolvesProblems@users.noreply.github.com>
2019-12-15 12:43:44 -0500

    Update Text Component: Added text dynamically explaing origin of the game, learned that can import Text field by using Unity.UI namespace. Putting [SerializeField] infront of text field will allow availability in the unity inspect. Can add text dynamically by setting the text component . the text property. Will appear once you click play, as putting it in the void Start() method

---
+++ Unity2D/Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // user interface namespace
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; // putting serializefield in front of variable, means now have available inside of inspector of unity
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // text will show when you click play in unity. Adding it dynamically
+        textComponent.text = ("Once upon a time long long ago, there was an army of soldiers doing everything they can to stop the war. THe war that would ultimately end humanity. The leader made a concious decision. Although the soldiers were out numbered. The leader burned the boat, so that there was no way back. The only way back was to win the war. Therefore, the soldiers had to fight for there life. After a long battle, they won that war. They kept going in hopes to see their families again and to this day 2019. We all perserve in peace because of that war.");   // access textCompoenent.test property
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
