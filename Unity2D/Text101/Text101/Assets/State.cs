51101c6e22b0022abd9852ccbbfbbc750193ef72
JonathanSolvesProblems <46333281+JonathanSolvesProblems@users.noreply.github.com>
2019-12-16 21:47:15 -0500

    Unity Scriptable Objects: played with scriptable objects in unity, puts comments of in-depth explanation in State.cs file

---
+++ Unity2D/Text101/Text101/Assets/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")] // for scriptableobjects
+public class State : ScriptableObject
+{
+    [TextArea(10,14)][SerializeField] string storyText; // TextArea(starting minumum, maximum can go with enter key)
+}
+
+/* 
+ * Can store data inside of scriptable objects then call them in our cs file to display the information.
+
+Normally have to attach every script to a game object, however, this rule does not apply for scriptable objects. If derived from MonoBehaviour class then has to add to game object
+
+    Id create a script object and derive it from ScriptableObject, CreateAssetMenu and set the name = to class name, will touch what that means later.
+
+    Remember SerializeField makes it available in the inspector. storyText, allows field in our scriptable object.
+
+    In unity, can do create, then name of class to make an object of that type.
+*/
